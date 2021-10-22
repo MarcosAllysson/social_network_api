8e1c544851f3dd01871b7f396c8879b5da99f7d3
MarcosAllysson <marcosgracip05@gmail.com>
2021-10-22 09:40:52 -0300

    configuring procfile

--- social_network_api/wsgi.py
+++ social_network_api/wsgi.py
@@ -1,5 +1,6 @@
 import os
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network_api.settings')
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
